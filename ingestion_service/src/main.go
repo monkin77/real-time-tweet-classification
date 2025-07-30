324394fe609bf30f946e5fd7b9fb4cd441790d92
Monkin <monkinsmurf@gmail.com>
2025-07-30 15:56:22 +0100

    Data Ingestion Client reading environment variables to setup Production V.S. Mock X API

--- ingestion_service/src/main.go
+++ ingestion_service/src/main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+// External libraries
 
 func main() {
 	// Print an hello message
 	println("Hello, Ingestion Service!")
 
-	// Initialize the X API Client (inline for now)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Print the requested path -- Response
-		fmt.Fprintf(w, "Hello, you've requested %s\n", r.URL.Path)
-	})
+	// Load environment variables and define the API URL based on the DEV_MOCK_STREAM flag.
+	godotenv.Load(".env") // Load the Environment Variables from the .env file
+	USE_MOCK_API := os.Getenv("DEV_MOCK_STREAM") == "true"
+	API_URL := "https://api.x.com"
+	if USE_MOCK_API {
+		port := os.Getenv("MOCK_API_PORT")
+		if port == "" {
+			log.Fatalf("MOCK_API_PORT is not set in the environment variables. Please set it in the .env file.")
+		}
+		API_URL = "http://localhost:" + port
+		log.Printf("Using Mock API for development. URL: %s\n", API_URL)
+	} else {
+		log.Printf("Using Live API for production. URL: %s\n", API_URL)
+	}
 
-	// Start the HTTP server
-	PORT := ":8080"
-	println("Server is running on port", PORT)
-	http.ListenAndServe(PORT, nil)
+	
 }
